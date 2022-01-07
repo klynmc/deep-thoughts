76e9cfc53a9b32b90a274e4c3286dbd0745d8e82
Leia <87383466+klynmc@users.noreply.github.com>
2022-01-07 11:21:24 -0600

    add client feature

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 
 const { ApolloServer } = require('apollo-server-express');
-const { typeDefs, resolvers } = require('./server/schemas');
-const { authMiddleware } = require('./server/utils/auth');
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
+const path = require('path');
 
-const db = require('./server/config/connection');
+const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -32,6 +33,15 @@ startServer();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Serve up static assets
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => {
