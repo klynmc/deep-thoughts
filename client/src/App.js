c6f4690ee6655a2335c7037b938e2af2bce046b4
Leia <87383466+klynmc@users.noreply.github.com>
2022-01-07 18:22:41 -0600

    add routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 
+import { BrowserRouter as /* Router, */ Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
+import NoMatch from './pages/NoMatch';
+import SingleThought from './pages/SingleThought';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
+
 import Header from './components/Header';
 import Footer from './components/Footer';
 
@@ -21,8 +28,16 @@ function App() {
       <div className="flex-column justify-flex-start min-100-vh">
         <Header />
         <div className="container">
-          <Home />
-        </div>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/signup" component={Signup} />
+            <Route exact path="/profile/:username?" component={Profile} />
+            <Route exact path="/thought/:id" component={SingleThought} />
+
+            <Route component={NoMatch} />
+            </Switch>
+          </div>
         <Footer />
       </div>
     </ApolloProvider>
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,18 +1,21 @@
 import {gql} from '@apollo/client';
 
 export const QUERY_THOUGHTS = gql `
-    query thoughts($username: String) {
-        thoughts(username: $username) {
-            _id
-            thoughtText
-            createdAt
-            username
-            reactionCount
-            reactions{
-                _id
-                createdAt
-                username
-                reactionBody
+    query user($username: String!) {
+        user(username: $username) {
+        _id
+        username
+        email
+        friendCount
+        friends {
+          _id
+          username
+        }
+        thoughts {
+          _id
+          thoughtText
+          createdAt
+          reactionCount
             }
         }
     }
