e59a4755b0bfefc5f4b8fd25aa6e346ce2893fbf
Leia <87383466+klynmc@users.noreply.github.com>
2022-01-09 18:56:12 -0600

    updated queries

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
 import { BrowserRouter as /* Router, */ Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import NoMatch from './pages/NoMatch';
 import SingleThought from './pages/SingleThought';
 import Profile from './pages/Profile';
-import Signup from './pages/Signup';
+import SignUp from './pages/SignUp';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -17,8 +18,18 @@ const httpLink = createHttpLink({
   uri: '/graphql',
 });
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
@@ -31,7 +42,7 @@ function App() {
           <Switch>
             <Route exact path="/" component={Home} />
             <Route exact path="/login" component={Login} />
-            <Route exact path="/signup" component={Signup} />
+            <Route exact path="/signup" component={SignUp} />
             <Route exact path="/profile/:username?" component={Profile} />
             <Route exact path="/thought/:id" component={SingleThought} />
 
